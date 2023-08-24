c44cb821354cc1de211cb8d6a2163e999c841ecb
Therese <thereseskold@live.com>
2023-08-24 15:10:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let countDownDate = new Date("may 18, 2024 18:00:00").getTime();
+
+let x = setInterval(function() {
+	let now = new Date().getTime();
+	let distance = countDownDate - now
+
+	let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
++ minutes + "m " + seconds + "s ";
+})
